c4bd14c4560c1cf3e3c0a6566b5968fca1d3f345
Austin Hunter <austin.hunter@payzer.com>
2021-02-17 15:05:21 -0600

    Fix audio

--- src/app/components/record-audio/record-audio.component.ts
+++ src/app/components/record-audio/record-audio.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { File } from '@ionic-native/file/ngx';
 import { Media } from '@ionic-native/media/ngx';
+import { Platform } from '@ionic/angular';
 import { StorageServiceService } from 'src/app/services/storage-service.service';
 import { LobbyBlockComponent } from '../lobby-block/lobby-block.component';
 
@@ -14,15 +15,24 @@ export class RecordAudioComponent implements OnInit {
   @Input() currentPlayer: any;
   recording = false;
   audioFile;
-  constructor(private media: Media, private file: File, private storage: StorageServiceService) { }
+  constructor(private media: Media, private file: File, private storage: StorageServiceService,
+    private platform: Platform) { }
 
   ngOnInit() {}
 
   async down() {
+    let filepath;
+    if (this.platform.is('ios')) filepath = this.file.tempDirectory;
+    else filepath = this.file.externalDataDirectory;
     this.recording = true;
-    await this.file.createFile(this.file.externalDataDirectory, 'voice.mp3', true);
-    this.audioFile = await this.media.create(this.file.externalDataDirectory + "voice.mp3");
+    await this.file.createFile(filepath, 'voice.m4a', true);
+    if (this.platform.is('ios')) {
+      filepath =this.file.tempDirectory.replace(/^file:\/\//, '');
+    }
+    this.audioFile = await this.media.create(filepath + "voice.m4a");
     this.audioFile.onSuccess.subscribe(async () => {
+      if (this.platform.is('ios')) filepath = this.file.tempDirectory;
+      else filepath = this.file.externalDataDirectory;
       let sender = "";
 
       if (this.currentPlayer.playerType == 1) {
@@ -38,7 +48,7 @@ export class RecordAudioComponent implements OnInit {
         sender: sender,
         text: ''
       }
-      const buffer = await this.file.readAsDataURL(this.file.externalDataDirectory, "voice.mp3");
+      const buffer = await this.file.readAsDataURL(filepath, "voice.m4a");
 
       let res = <any>await this.storage.uploadAudioMessage(obj, buffer);
       let downloadUrl = await res.ref.getDownloadURL();
