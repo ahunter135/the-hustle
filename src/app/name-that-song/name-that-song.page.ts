ff4db8c43d7681116bc52a7149e85de3bc826c8a
ahunter135 <ahunter8850@outlook.com>
2021-05-12 09:51:58 -0500

    no message

--- src/app/name-that-song/name-that-song.page.ts
+++ src/app/name-that-song/name-that-song.page.ts
@@ -1,5 +1,8 @@
+import { AdMob } from '@admob-plus/ionic';
 import { Component, OnInit } from '@angular/core';
-import { LoadingController } from '@ionic/angular';
+import { Router } from '@angular/router';
+import { OneSignal } from '@ionic-native/onesignal/ngx';
+import { LoadingController, Platform } from '@ionic/angular';
 
 @Component({
   selector: 'app-name-that-song',
@@ -9,7 +12,7 @@ import { LoadingController } from '@ionic/angular';
 export class NameThatSongPage implements OnInit {
   loader;
   isLoading = false;
-  constructor(public loadingController: LoadingController) { }
+  constructor(public loadingController: LoadingController, private platform: Platform, private admob: AdMob, private router: Router, private onesignal: OneSignal) { }
 
   async ngOnInit() {
     await this.presentLoading();
@@ -32,5 +35,51 @@ export class NameThatSongPage implements OnInit {
 
   async getGameData() {
     // call http services to get all the game data necessary
+  }
+  
+  async cancel() {
+    await this.showAdAndLeave();
+  }
+
+  showAdAndLeave() {
+    if (!this.platform.is('cordova')) {
+      this.router.navigateByUrl("/home", {
+        replaceUrl: true
+      });
+      return
+    }
+    this.presentLoading();
+
+    this.admob.interstitial.load({
+      id: {
+        android: 'ca-app-pub-8417638044172769/2470631346',
+        ios: 'ca-app-pub-8417638044172769/1204515667'
+      }
+    }).then((res) => {
+      this.admob.interstitial.show().then(() => {
+        this.dismissLoading();
+        this.onesignal.addTrigger('leave', true);
+        this.router.navigateByUrl("/home", {
+          replaceUrl: true
+        });
+      }, (reason) => {
+        console.log(reason);
+        this.dismissLoading();
+          this.onesignal.addTrigger('leave', true);
+        this.router.navigateByUrl("/home", {
+          replaceUrl: true
+        });
+      });
+    }, (reason) => {
+      console.log(reason);
+      this.dismissLoading();
+      this.onesignal.addTrigger('leave', true);
+      this.router.navigateByUrl("/home", {
+        replaceUrl: true
+      });
+    }).catch(() => {
+      this.dismissLoading();
+    });
+    return;
   }
 }
