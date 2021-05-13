d4011bba1f6dfb378ff3b23e79d80b38a7760549
ahunter135 <ahunter8850@outlook.com>
2021-05-13 10:45:59 -0500

    no message

--- src/app/services/global.service.ts
+++ src/app/services/global.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Subject } from 'rxjs';
-
+import stringSimilarity from 'string-similarity';
 @Injectable({
   providedIn: 'root'
 })
@@ -16,5 +16,19 @@ export class GlobalService {
 
   getObservable(): Subject<any> {
     return this.fooSubject;
+  }
+
+  determineCorrectAnswer(answer, guess) {
+    let songMatch = stringSimilarity.findBestMatch(answer.title, guess);
+    let artistMatch = stringSimilarity.findBestMatch(answer.artist, guess);
+    let bestSongMatch = songMatch.bestMatch.rating;
+    let bestArtistMatch = artistMatch.bestMatch.rating;
+
+    console.log(songMatch);
+    console.log(artistMatch);
+    return {
+      artist: bestArtistMatch >= 0.75,
+      song: bestSongMatch >= 0.75
+    };
   }
 }
